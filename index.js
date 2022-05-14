130cd69101b5da42a1091555a5b9b43f9cd6605d
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-05-14 13:15:23 +0200

    Add dl-clickable dropdown

---
+++ index.js
@@ -0,0 +1,15 @@
+const def_clickable = document.getElementById("def_clickable");
+const def_dt = def_clickable.querySelectorAll("dt");
+
+
+// **** Toggle def_clickable **** //
+
+def_clickable.addEventListener("click", (val) => {
+  def_dt.forEach((e) => {
+    if (e == val.target) {
+      e.classList.toggle("opened");
+    } else {
+      e.classList.remove("opened");
+    }
+  });
+});
