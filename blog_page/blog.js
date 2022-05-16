bcd67ce8a690fffd14845298ce77dc0ec57d24fb
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-05-16 14:10:16 +0200

    Add readmore

--- blog_page/blog.js
+++ blog_page/blog.js
@@ -3,6 +3,15 @@ const articles = document.querySelectorAll('.apt_block');
 
 articles.forEach(e => {
   let entry_content = e.querySelector('.entry_content');
-  let cn = entry_content.textContent.slice(0, 600) + '...';
-  entry_content.textContent = cn;
+  let cn = entry_content.textContent;
+  if(cn.length > 600) {
+   cn = entry_content.textContent.slice(0, 600) + '...';
+
+   let btn_more = document.createElement("a");
+   btn_more.setAttribute('href', '/');
+   btn_more.classList.add('btn', 'btn_more');
+   btn_more.innerText = 'Read more';
+   entry_content.appendChild(btn_more);
+  }
+  entry_content.querySelector('p').textContent = cn;
 })
