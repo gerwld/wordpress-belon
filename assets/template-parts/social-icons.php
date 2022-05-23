5761867ea2a9b7dda1cf3f84a1fec86bd7e57121
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-05-23 03:39:31 +0200

    Minor changes

--- assets/template-parts/social-icons.php
+++ assets/template-parts/social-icons.php
@@ -34,17 +34,17 @@
   </svg>
  </a></li>'
   : ''; ?>
-   <?php echo $social_options['instagram'] ?
+ <?php echo $social_options['instagram'] ?
   '<li class="ig"><a href="' . $social_options['instagram'] . '" target="_blank" and rel="noopener noreferrer">
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="transparent" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
  </a></li>'
   : ''; ?>
-    <?php echo $social_options['dribble'] ?
+ <?php echo $social_options['dribble'] ?
   '<li class="db"><a href="' . $social_options['dribble'] . '" target="_blank" and rel="noopener noreferrer">
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"  fill="transparent" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-dribbble"><circle cx="12" cy="12" r="10"></circle><path d="M8.56 2.75c4.37 6.03 6.02 9.42 8.03 17.72m2.54-15.38c-3.72 4.35-8.94 5.66-16.88 5.85m19.5 1.9c-3.5-.93-6.63-.82-8.94 0-2.58.92-5.01 2.86-7.44 6.32"></path></svg>
  </a></li>'
   : ''; ?>
-      <?php echo $social_options['github'] ?
+ <?php echo $social_options['github'] ?
   '<li class="gh"><a href="' . $social_options['github'] . '" target="_blank" and rel="noopener noreferrer">
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"  fill="transparent" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
  </a></li>'
